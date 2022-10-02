249c07a060fe8e4e83bfeaebd44012c4760aadef
Miguel Castaneda <miguelcastaneda@Miguels-MBP-3.home>
2022-10-02 06:40:19 -0700

    styling

--- react-tailwindcss-portfolio-master/tailwind.config.js
+++ react-tailwindcss-portfolio-master/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ["./src/**/*.{html,js}"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
