e81e08497065bffd09b1fcbd1cb88ce2804c8d3b
Miguel Castaneda <miguelcastaneda@Miguels-MBP-3.home>
2022-10-01 18:25:00 -0700

    first commit

---
+++ react-tailwindcss-portfolio-master/src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "dustLoops",
+    subtitle: "Ruby/Ruby on Rails",
+    description:
+      "Sample database app with personalized list capabilities",
+    image: "./giphy4.gif",
+    link: "https://murmuring-coast-08834.herokuapp.com/",
+  },
+  {
+    title: "Techy",
+    subtitle: "Python, Django, React",
+    description:
+      "Tech resale app -- users are able to manage their accounts and list specific items for sale",
+    image: "./giphy3.gif",
+    link: "#",
+  },
+  {
+    title: "BOS2LAX",
+    subtitle: "React, Express, Node, MongoDB Atlas",
+    description:
+      "Travel suggestion guide with the ability to add to public listings for each respective city",
+    image: "./giphy2.gif",
+    link: "https://arcane-garden-99958.herokuapp.com",
+  },
+  {
+    title: "Coffee Compendium",
+    subtitle: "MongoDB, Express, EJS",
+    description:
+      "Allows users to submit their own coffee drink recipes and to edit existing ones",
+    image: "./giphy1.gif",
+    link: "https://fathomless-escarpment-41920.herokuapp.com/drinks",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
