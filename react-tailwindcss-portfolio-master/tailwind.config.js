b367088f96b2acb9907c598b0fc440655f85447e
Miguel Castaneda <miguelcastaneda@Miguels-MBP-3.lan>
2022-10-02 19:37:49 -0700

    commit

--- react-tailwindcss-portfolio-master/tailwind.config.js
+++ react-tailwindcss-portfolio-master/tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  purge: {
-    enabled: true,
-    content: [
-      './**/*.html'
-    ]
-  },
   theme: {
       fontFamily: {
         'sans': ['Work Sans', 'sans-serif']
